b6e089ea7e8c8111557ffe7e67ae2e3bc9fdee69
Alexander Polyakov <AAlPolyakov@sberbank.ru>
2022-04-07 23:33:05 +0300

    added seed data

--- server/src/seed/1-reset.sql
+++ server/src/seed/1-reset.sql
@@ -10,8 +10,16 @@ alter sequence task_answer_comments_id_seq restart with 1;
 
 insert into students (name) values ('ivan testovich');
 insert into students (name) values ('test petrovich');
-insert into tasks (title, description) values ('test title', 'test desc');
-insert into tasks (title, description) values ('second task', 'another desc');
+insert into tasks (title, description) values ('Learning Python', 'Python task description');
+insert into tasks (title, description) values ('Getting started with TypeScript', 'TS task description');
+insert into tasks (title, description) values ('Hello world with Rust', 'Rust task description');
+insert into tasks (title, description) values ('JavaScript', 'JS task description');
+insert into tasks (title, description) values ('Golang', 'Something about golang');
+insert into tasks (title, description) values ('C', 'Old school');
+insert into tasks (title, description) values ('C++', 'C++ task description');
+insert into tasks (title, description) values ('Java', 'Java task description');
+insert into tasks (title, description) values ('Scala', 'Scala task description');
+insert into tasks (title, description) values ('Brainfuck', '...');
 insert into task_answers (id_task, id_student, content) values (1, 1, 'test content');
 insert into task_answers (id_task, id_student, content) values (2, 1, 'test content by ivan testovich');
 insert into task_answers (id_task, id_student, content) values (2, 2, 'test content by test petrovich');
